c7d592d11fb16c260d45f33335acf154ee13f012
Kamil <kamil@nowinski.net>
2020-05-14 22:03:27 +0100

    Build script

--- build.ps1
+++ build.ps1
@@ -5,14 +5,14 @@ $verarr = $ver.Split('.')
 Write-Output "Module version: $ver"
 
 $taskFile = Join-Path -Path (Get-Location) -ChildPath 'deployDataFactoryTask\task.json'
-Copy-Item -Path $taskFile -Destination "$taskFile - copy"
+#Copy-Item -Path $taskFile -Destination "$taskFile - copy"
 Write-Output "Updating version for task definition in file: $taskFile"
 $JsonDoc = Get-Content $taskFile -Raw | ConvertFrom-Json
 $JsonDoc.version.Major = $verarr[0]
 $JsonDoc.version.Minor = $verarr[1]
 $JsonDoc.version.Patch = $verarr[2]
 $JsonDoc | ConvertTo-Json | Out-File "$taskFile" -Encoding utf8
-
+Write-Output "File updated."
 
 
 

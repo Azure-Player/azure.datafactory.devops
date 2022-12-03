f7db411b2e6119be85c1aac1e9ecb7abf2d00887
Kamil Nowinski <kamil@nowinski.net>
2022-12-03 01:03:32 +0000

    Update-modules script + update of #adftools to ver.0.110

--- update-modules.ps1
+++ update-modules.ps1
@@ -1,7 +1,7 @@
-- [azure.datafactory.tools - ver.0.97.0](https://www.powershellgallery.com/packages/azure.datafactory.tools/0.97.0)
-- [Az.DataFactory - ver.1.16.6](https://www.powershellgallery.com/packages/Az.DataFactory/1.16.6)
-- [Az.Accounts - ver.2.5.3](https://www.powershellgallery.com/packages/Az.Accounts/2.5.3)
-- [Az.Resources - ver.4.3.1](https://www.powershellgallery.com/packages/Az.Resources/4.3.1)
+# [azure.datafactory.tools - ver.0.97.0](https://www.powershellgallery.com/packages/azure.datafactory.tools/0.97.0)
+# [Az.DataFactory - ver.1.16.6](https://www.powershellgallery.com/packages/Az.DataFactory/1.16.6)
+# [Az.Accounts - ver.2.5.3](https://www.powershellgallery.com/packages/Az.Accounts/2.5.3)
+# [Az.Resources - ver.4.3.1](https://www.powershellgallery.com/packages/Az.Resources/4.3.1)
 
 $target = 'd:\modules'
 Remove-Item -Path "$target\*" -Force
@@ -18,16 +18,55 @@ function Download-Module {
     $mod
 }
 
+function Update-ModuleInPlace {
+    param ([String] $m, [String[]]$tasks)
+
+    $targetPath = (.\Get-RootPath.ps1)
+    $src = Join-Path $target $m
+
+    foreach ($t in $tasks) {
+        $short = $t.EndsWith('*')
+        $t = $t.Replace('*', '')
+        $dst = "$targetPath\$t\ps_modules"
+        Write-Host "Module: $src"
+        Write-Host "- copy to: $dst ..."
+        Copy-Item -Path $src -Destination $dst -Recurse -Force
+        if ($short) {
+            $file = "$dst\$m\$m.psm1"
+            Write-Host "- modifying file: $file ..."
+            $lines = Get-Content -Path $file -Encoding utf8
+            $sb = [System.Text.StringBuilder]::new()
+            $commentActive = $false
+            foreach ($line in $lines) {
+                if ($line.StartsWith('$moduleName = ')) { $commentActive = $true }
+                if ($commentActive) { $line = "# $line" }
+                $sb.AppendLine($line) | Out-Null
+            }
+            $sb.ToString() | Set-Content -Path $file
+        }
+    }
+}
 
 
-$m = 'azure.datafactory.tools'
-Download-Module -m $m -target $target
+# Downloading modules
+$m = 'azure.datafactory.tools'; Download-Module -m $m -target $target
+$m = 'Az.DataFactory';          Download-Module -m $m -target $target
+$m = 'Az.Accounts';             Download-Module -m $m -target $target
+$m = 'Az.Resources';            Download-Module -m $m -target $target
 
-$m = 'Az.DataFactory'
-Download-Module -m $m -target $target
+# Updating modules in 'azure.datafactory.devops'
+$m = 'azure.datafactory.tools'; 
+$tasks = @('buildDataFactoryTask*', 'deployAdfFromArmTask*', 'deployDataFactoryTask', 'testLinkedServiceTask*')
+Update-ModuleInPlace $m $tasks
 
-$m = 'Az.Accounts'
-Download-Module -m $m -target $target
+$m = 'Az.DataFactory';
+$tasks = @('deployDataFactoryTask')
+Update-ModuleInPlace $m $tasks
 
-$m = 'Az.Resources'
-Download-Module -m $m -target $target
+$m = 'Az.Accounts';
+$tasks = @('deployDataFactoryTask', 'testLinkedServiceTask')
+Update-ModuleInPlace $m $tasks
+
+$m = 'Az.Resources';
+$tasks = @('deployDataFactoryTask', 'testLinkedServiceTask')
+Update-ModuleInPlace $m $tasks

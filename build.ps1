31321ecd2686b2b3991867be7e46c422d3fc626b
Kamil Nowinski <kamil@nowinski.net>
2024-06-14 10:26:28 +0100

    Fix version in build script

--- build.ps1
+++ build.ps1
@@ -19,8 +19,8 @@ function Update-TaskManifest {
     $body = $JsonDoc = Get-Content $taskFile -Raw
     $JsonDoc = $body | ConvertFrom-Json
     #$body = $body.Replace('"Major": '+$JsonDoc.version.Major, '"Major": '+$verarr[0])
-    $body = $body.Replace('"Minor": '+$JsonDoc.version.Minor, '"Minor": '+$verarr[1])
-    $body = $body.Replace('"Patch": '+$JsonDoc.version.Patch, '"Patch": '+$verarr[2])
+    $body = $body.Replace('"Minor": '+$JsonDoc.version.Minor, '"Minor": '+$version.Minor)
+    $body = $body.Replace('"Patch": '+$JsonDoc.version.Patch, '"Patch": '+$version.Build)
     if (!$IsProd) {
         Write-Output "- Updating task id and friendly name for private preview..."
         $TaskId = $JsonDoc.id
@@ -44,8 +44,11 @@ $body = Get-Content $vssFile -Raw
 $JsonDoc = $body | ConvertFrom-Json
 $ver = $JsonDoc.Version
 $verarr = $ver.Split('.')
-$version = [System.Version]::new($verarr[0], $verarr[1], $build)
+$version2 = [System.Version]::new($verarr[0], $verarr[1], $build)
+$version1 = [System.Version]::new(1, 35, $build)
 Write-Output "Module version: $ver"
+Write-Output "Updated version1: $version1"
+Write-Output "Updated version2: $version2"
 
 # Replace with regex
 $v = '"version": "'+$JsonDoc.Version+'",'
@@ -62,16 +65,14 @@ $body | Out-File "$vssFile" -Encoding utf8
 Write-Output "File vss updated."
 
 # Update task definitions
-Update-TaskManifest -taskFolder 'deployDataFactoryTaskV2' -version $version -NonProdTaskId 'b2032481-f9a9-476d-af8f-9156ee066e1b' -isProd $isProd
-Update-TaskManifest -taskFolder 'buildDataFactoryTaskV2'  -version $version -NonProdTaskId '0fff6fc0-4a02-46b2-9466-2ee2a9b0580f' -isProd $isProd
-Update-TaskManifest -taskFolder 'testLinkedServiceTaskV2' -version $version -NonProdTaskId '9cb687ea-a4f1-45d5-a568-98dc85fd3f1b' -isProd $isProd
-Update-TaskManifest -taskFolder 'deployAdfFromArmTaskV2'  -version $version -NonProdTaskId 'f38a9662-edd3-4af5-b4c2-35f4d3e31dda' -isProd $isProd
-
-$version = [System.Version]::new(1, 35, $build)
-Update-TaskManifest -taskFolder 'deployDataFactoryTaskV1' -version $version -NonProdTaskId 'b2032481-f9a9-476d-af8f-9156ee066e1b' -isProd $isProd
-Update-TaskManifest -taskFolder 'buildDataFactoryTaskV1'  -version $version -NonProdTaskId '0fff6fc0-4a02-46b2-9466-2ee2a9b0580f' -isProd $isProd
-Update-TaskManifest -taskFolder 'testLinkedServiceTaskV1' -version $version -NonProdTaskId '9cb687ea-a4f1-45d5-a568-98dc85fd3f1b' -isProd $isProd
-Update-TaskManifest -taskFolder 'deployAdfFromArmTaskV1'  -version $version -NonProdTaskId 'f38a9662-edd3-4af5-b4c2-35f4d3e31dda' -isProd $isProd
+Update-TaskManifest -taskFolder 'deployDataFactoryTaskV2' -version $version2 -NonProdTaskId 'b2032481-f9a9-476d-af8f-9156ee066e1b' -isProd $isProd
+Update-TaskManifest -taskFolder 'buildDataFactoryTaskV2'  -version $version2 -NonProdTaskId '0fff6fc0-4a02-46b2-9466-2ee2a9b0580f' -isProd $isProd
+Update-TaskManifest -taskFolder 'testLinkedServiceTaskV2' -version $version2 -NonProdTaskId '9cb687ea-a4f1-45d5-a568-98dc85fd3f1b' -isProd $isProd
+Update-TaskManifest -taskFolder 'deployAdfFromArmTaskV2'  -version $version2 -NonProdTaskId 'f38a9662-edd3-4af5-b4c2-35f4d3e31dda' -isProd $isProd
+Update-TaskManifest -taskFolder 'deployDataFactoryTaskV1' -version $version1 -NonProdTaskId 'b2032481-f9a9-476d-af8f-9156ee066e1b' -isProd $isProd
+Update-TaskManifest -taskFolder 'buildDataFactoryTaskV1'  -version $version1 -NonProdTaskId '0fff6fc0-4a02-46b2-9466-2ee2a9b0580f' -isProd $isProd
+Update-TaskManifest -taskFolder 'testLinkedServiceTaskV1' -version $version1 -NonProdTaskId '9cb687ea-a4f1-45d5-a568-98dc85fd3f1b' -isProd $isProd
+Update-TaskManifest -taskFolder 'deployAdfFromArmTaskV1'  -version $version1 -NonProdTaskId 'f38a9662-edd3-4af5-b4c2-35f4d3e31dda' -isProd $isProd
 
 # Build extension
 tfx extension create --manifest-globs vss-extension.json --output-path ./bin

0ac1ab6800634916bddc45f6b650192173581c2e
Kamil Nowinski <kamil@nowinski.net>
2025-06-03 20:08:07 +0100

    Refactor Azure Data Factory Tools: Update module download script and add Set-AdfToolsAuthToken function
    - Commented out Az.Accounts and Az.Resources module downloads in update-modules.ps1.
    - Added Set-AdfToolsAuthToken function to multiple modules for setting the base API URL.
    - Implemented detailed documentation for Set-AdfToolsAuthToken function, including synopsis, description, parameters, examples, and notes.
    - Created .signature.p7s file for Az.DataFactory module.

--- testLinkedServiceTask/testLinkedServiceTaskV2/ps_modules/azure.datafactory.tools/private/Get-AzDFV2Credential.ps1
+++ testLinkedServiceTask/testLinkedServiceTaskV2/ps_modules/azure.datafactory.tools/private/Get-AzDFV2Credential.ps1
@@ -5,19 +5,17 @@ function Get-AzDFV2Credential {
     )
     Write-Debug "BEGIN: Get-AzDFV2Credential"
 
-    # Retrieve all credentials via API without parsing
-    $token = Get-AzAccessToken -ResourceUrl 'https://management.azure.com'
-    $authHeader = @{
-        'Content-Type'  = 'application/json'
-        'Authorization' = 'Bearer ' + $token.Token
-    }
-    $url = "https://management.azure.com$($adfi.DataFactoryId)/credentials?api-version=2018-06-01"
+    $url = "$($script:BaseApiUrl)$($adfi.DataFactoryId)/credentials?api-version=2018-06-01"
 
     # Retrieve all credentials via Rest API
-    $r = Invoke-RestMethod -Method 'GET' -Uri $url -Headers $authHeader -ContentType "application/json"
+    $r = Invoke-AzRestMethod -Method 'GET' -Uri $url
+    if ($r.StatusCode -ne 200) {
+        Write-Error -Message "Unexpected response code: $($r.StatusCode) from the API."
+        return $null
+    }
 
     [System.Collections.ArrayList] $items = @{}
-    $r.value | ForEach-Object { $i = [AdfPSCredential]::New($_); $items.Add($i) | Out-Null; }
+    ($r.Content | ConvertFrom-Json).value | ForEach-Object { $i = [AdfPSCredential]::New($_); $items.Add($i) | Out-Null; }
 
     Write-Debug "END: Get-AzDFV2Credential()"
     return $items
